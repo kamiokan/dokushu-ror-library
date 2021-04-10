321428c82d4f57a169649544ad7e7d5e6c518d37
kamiokan <spherenov@gmail.com>
2021-04-10 11:15:00 +0900

    6.1.7 フォームオブジェクトの簡単な例 まで

---
+++ app/controllers/acceptances_controller.rb
@@ -0,0 +1,21 @@
+class AcceptancesController < ApplicationController
+  def new
+    @acceptance = Acceptance.new
+  end
+
+  def create
+    @acceptance = Acceptance.new(acceptances_params)
+    if @acceptance.valid?
+      puts "承諾されました！！"
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def acceptances_params
+    params.require(:acceptance).permit(:accepting)
+  end
+end
